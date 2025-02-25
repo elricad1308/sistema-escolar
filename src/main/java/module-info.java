102d74d5561de285e88886cf8750beb721476896
José Aguilar <jc.aguilar1308@gmail.com>
2025-02-24 19:18:34 -0600

    feat: :construction: Modificado module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -10,5 +10,5 @@ module SistemaEscolar {
     // nuestro paquete 'ejemplo'
     opens mx.uacam.fi.se.javafx to javafx.fxml, javafx.graphics;
     opens mx.uacam.fi.se.modelos to com.google.gson;
-    opens mx.uacam.fi.se.data to com.google.gson;
+    opens mx.uacam.fi.se.data to com.google.gson, javafx.base;
 }
